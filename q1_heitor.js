ac51b3d48cffae52241c54947061315494a322ed
heitorcostella <heitorcostella.pf007@academico.ifsul.edu.br>
2026-04-14 15:39:17 -0300

    Atividade q1 quase pronta

---
+++ q1_heitor.js
@@ -0,0 +1,16 @@
+function solucao() {
+    var numero = parseInt(document.getElementById('numero').value); 
+         if( numero%3 === 0 & numero%5 === 0){ 
+      alert("FizzBuzz");
+      return;
+}
+     if( numero%3 === 0){ 
+      alert("Fizz");
+}
+     if( numero%5 === 0){ 
+      alert("Buzz");
+}
+     if( numero%7 === 0){ 
+      alert("Boom");
+}
+}
