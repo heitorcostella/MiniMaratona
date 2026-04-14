d15ce458eea5d722241c1160cb2d4ef5c3faefda
não interessa <augustotibola@gmail.com>
2026-04-14 15:35:50 -0300

    Não fiz nada
    A professora fez tudo (disse o que eu tinha que fazer), 0 mérito, partiu Cecy ou EENAV

--- teste_tibola.js
+++ teste_tibola.js
@@ -1,1 +1,14 @@
-menubar, bmn 
+function teste(vetor){
+
+    let vetorAuxiliar = [];
+
+
+
+    for(let i = vetor.length - 1; i >= 0; i--){
+
+        vetorAuxiliar.push(vetor[i])
+    }
+return vetorAuxiliar
+
+
+}
